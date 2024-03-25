798fd240eb41a3eb300f5bdd243eff359015e7b6
laylabtrice <layla.beatrice@alunoescolassociais.org.br>
2024-03-25 12:03:58 -0300

    atualizações sem o uso do css presentes no código

--- README.md
+++ README.md
@@ -7,8 +7,7 @@
 * a
 
 # sistema_salario
- Sistema em que solicita o nome do vendedor, os valores de meta semanal e mensal, e, ao final, o sistema calculará o salário final e irá exibir ao usuário;
+ Sistema em que solicita o nome do vendedor, os valores de meta semanal e mensal, e, ao final, o sistema calculará o salário final e irá exibi-lo ao usuário, para que assim o vendedor saiba seu salário final ao mês;
 
 ## Autores
-
-Layla Beatrice Ribeiro
+ * Layla Beatrice Ribeiro
--- cod.php
+++ cod.php
@@ -3,22 +3,28 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Sistema Salário</title>
+    <title>Sistema Salário Vendedor</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
 </head>
 <body>
     <span>Informações</span>
-    <div>
-    <div class="item">
-    <form action="" method="form">
-    <input type="number" name="numero"><br>
-    <input type="number" name="numero"><br>
-    <input type="number" name="numero"><br>
-    <input type="number" name="numero"><br>
-    <br><br>
-    <button type="button" onclick="validar()">Enviar!</button>
-    </div>
+      <!--nome vendedor-->
+      <h1>Informe os dados pedidos</h1>
+    <form method="post">
+    <label for="name">NOME:</label>
+    <input type="name" name="name" />
+
+    <!--valor meta semanal (20 mil reais)-->
+    <label for="name">VALOR DA META SEMANAL:</label>
+    <input type="number" name="salario" />
+
+    <!--valor meta mensal (80 mil reais)-->
+    <label for="name">VALOR DA META MENSAL:</label>
+    <input type="number" name="salario1" />
+    
+    <input type="submit" name="submit" value="Calcular" />
+    </form>
     <?php
     
     ?>
